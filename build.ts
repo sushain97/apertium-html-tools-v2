b6a9a14a89edadc7253f91aa32acc27426528745
Sushain Cherivirala <sushain@skc.name>
2021-04-04 13:35:09 -0700

    Working e2e

---
+++ build.ts
@@ -0,0 +1,13 @@
+import { promises as fs } from 'fs';
+import * as path from 'path';
+
+const DIST = 'dist/';
+
+(async () => {
+  await fs.copyFile('src/index.html', path.join(DIST, 'index.html'));
+  await require('esbuild').build({
+    entryPoints: ['src/app.tsx'],
+    bundle: true,
+    outfile: path.join(DIST, 'bundle.js'),
+  });
+})();
