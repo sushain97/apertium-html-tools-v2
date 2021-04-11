35eb84a3dec900b4d8845cc16f651386847efe64
Sushain Cherivirala <sushain@skc.name>
2021-04-10 22:05:41 -0700

    Footer with enum

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -8,11 +8,24 @@ import AboutModal from './AboutModal';
 import DownloadModal from './DownloadModal';
 import DocumentationModal from './DocumentationModal';
 import ContactModal from './ContactModal';
+import { ModalProps } from 'react-bootstrap/Modal';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const version: string = (window as any).VERSION;
 
-type Tab = 'about' | 'download' | 'documentation' | 'contact';
+const enum Tab {
+  About = 'about',
+  Download = 'download',
+  Documentation = 'documentation',
+  Contact = 'contact',
+}
+
+const Tabs: { [tab in Tab]: (props: ModalProps) => React.ReactElement } = {
+  [Tab.About]: AboutModal,
+  [Tab.Download]: DownloadModal,
+  [Tab.Documentation]: DocumentationModal,
+  [Tab.Contact]: ContactModal,
+};
 
 const Footer = ({
   wrapRef,
@@ -46,22 +59,26 @@ const Footer = ({
           <div className="d-none d-md-flex flex-wrap flex-row justify-content-between position-relative row">
             <Nav variant="pills" as="ul" style={{ cursor: 'pointer' }}>
               <Nav.Item as="li">
-                <a tabIndex={0} onClick={() => setOpenTab('about')} className="footer-link">
+                <a tabIndex={0} onClick={() => setOpenTab(Tab.About)} className="footer-link">
                   <i className="fa fa-question-circle"></i> {t('About')}
                 </a>
               </Nav.Item>
               <Nav.Item as="li">
-                <a tabIndex={0} onClick={() => setOpenTab('download')} className="footer-link">
+                <a tabIndex={0} onClick={() => setOpenTab(Tab.Download)} className="footer-link">
                   <i className="fa fa-download"></i> {t('Download')}
                 </a>
               </Nav.Item>
               <Nav.Item as="li">
-                <a tabIndex={0} onClick={() => setOpenTab('documentation')} className="footer-link">
+                <a
+                  tabIndex={0}
+                  onClick={() => setOpenTab(Tab.Documentation)}
+                  className="footer-link"
+                >
                   <i className="fa fa-book"></i> {t('Documentation')}
                 </a>
               </Nav.Item>
               <Nav.Item as="li">
-                <a tabIndex={0} onClick={() => setOpenTab('contact')} className="footer-link">
+                <a tabIndex={0} onClick={() => setOpenTab(Tab.Contact)} className="footer-link">
                   <i className="fa fa-envelope"></i> {t('Contact')}
                 </a>
               </Nav.Item>
@@ -69,7 +86,7 @@ const Footer = ({
 
             <div className="card d-inline-block bg-light mr-3 mb-4 ml-3 p-2">
               <span>{t('Notice_Mistake')}</span>{' '}
-              <a tabIndex={0} onClick={() => setOpenTab('about')} style={{ cursor: 'pointer' }}>
+              <a tabIndex={0} onClick={() => setOpenTab(Tab.About)} style={{ cursor: 'pointer' }}>
                 {t('Help_Improve')}
               </a>
             </div>
@@ -86,14 +103,13 @@ const Footer = ({
         </div>
       </div>
 
-      <AboutModal show={openTab === 'about'} onHide={() => setOpenTab(undefined)} />
-      <DownloadModal show={openTab === 'download'} onHide={() => setOpenTab(undefined)} />
-      <DocumentationModal show={openTab === 'documentation'} onHide={() => setOpenTab(undefined)} />
-      <ContactModal show={openTab === 'contact'} onHide={() => setOpenTab(undefined)} />
+      {Object.entries(Tabs).map(([tab, Modal]) => (
+        <Modal key={tab} show={openTab === tab} onHide={() => setOpenTab(undefined)} />
+      ))}
 
       <div className="align-self-end card card-body d-block bg-light d-md-none mt-2 mr-0 mb-0 p-2">
         <span>{t('Notice_Mistake')}</span>
-        <a tabIndex={0} onClick={() => setOpenTab('about')} style={{ cursor: 'pointer' }}>
+        <a tabIndex={0} onClick={() => setOpenTab(Tab.About)} style={{ cursor: 'pointer' }}>
           {t('Help_Improve')}
         </a>
       </div>
