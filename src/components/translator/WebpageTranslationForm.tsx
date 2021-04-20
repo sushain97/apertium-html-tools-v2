1406cb422da989ea43bd552e8f71d47418cbfc37
Sushain Cherivirala <sushain@skc.name>
2021-04-19 23:07:17 -0700

    Start on web translation UI

--- src/components/translator/WebpageTranslationForm.tsx
+++ src/components/translator/WebpageTranslationForm.tsx
@@ -1,14 +1,41 @@
 import * as React from 'react';
+import { faArrowLeft, faLink } from '@fortawesome/free-solid-svg-icons';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
 
-const WebpageTranslationForm = (props: {
+import { useLocalization } from '../../util/localization';
+
+const WebpageTranslationForm = ({
+  cancelLink,
+}: {
   cancelLink: string;
   srcLang: string;
   tgtLang: string;
 }): React.ReactElement => {
+  const { t } = useLocalization();
+
   return (
-    <>
-      TODO: {props.srcLang}-{props.tgtLang}
-    </>
+    <Row>
+      <Col sm="12">
+        <div className="d-inline-flex mb=2 w-100">
+          <Button href={cancelLink} variant="secondary">
+            <FontAwesomeIcon icon={faArrowLeft} /> {t('Cancel')}
+          </Button>
+          <InputGroup className="ml-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text>
+                <FontAwesomeIcon icon={faLink} />
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control autoComplete="url" placeholder="URL ⏎" required />
+          </InputGroup>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
