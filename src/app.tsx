ec7710ced5c37b3192e94840340da514f1d3b7a1
Sushain Cherivirala <sushain@skc.name>
2021-05-01 15:37:10 -0700

    Make strings requests relative

--- src/app.tsx
+++ src/app.tsx
@@ -101,7 +101,7 @@ const App = () => {
     void (async () => {
       let localeStrings: Strings;
       try {
-        localeStrings = (await axios({ url: `/strings/${locale}.json`, validateStatus: (status) => status === 200 }))
+        localeStrings = (await axios({ url: `strings/${locale}.json`, validateStatus: (status) => status === 200 }))
           .data as Strings;
       } catch (error) {
         console.warn(`Failed to fetch strings, falling back to default ${Config.defaultLocale}`, error);
