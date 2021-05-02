b294ec5226ccf5011ff136e587df6a479e91bcc5
Sushain Cherivirala <sushain@skc.name>
2021-05-02 10:25:42 -0700

    DRY

--- src/components/LocaleSelector.tsx
+++ src/components/LocaleSelector.tsx
@@ -5,6 +5,8 @@ import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { LocaleContext } from '../context';
 import locales from '../strings/locales.json';
 
+const height = '1.5rem';
+
 const LocaleSelector = ({
   setLocale,
   inverse,
@@ -15,24 +17,13 @@ const LocaleSelector = ({
   const locale = React.useContext(LocaleContext);
 
   return (
-    <div className="mt-2">
-      <FontAwesomeIcon
-        className="float-right ml-2"
-        icon={faGlobe}
-        inverse={inverse}
-        style={{
-          fontSize: '25px',
-          height: '25px',
-        }}
-      />
+    <div className="mt-2" style={{ height }}>
+      <FontAwesomeIcon className="float-right ml-2" icon={faGlobe} inverse={inverse} style={{ fontSize: height }} />
       {/* eslint-disable-next-line jsx-a11y/no-onchange */}
       <select
-        className="float-right"
+        className="float-right h-100"
         onChange={({ target: { value } }) => setLocale(value)}
-        style={{
-          fontSize: '14px',
-          height: '25px',
-        }}
+        style={{ fontSize: '0.9rem' }}
         value={locale}
       >
         {Object.entries(locales)
