ede0447396ecc34b9b69a6930341c1d176d2dc9d
Sushain Cherivirala <sushain@skc.name>
2021-05-02 13:14:24 -0700

    Switch to esbuild plugin API

--- build.ts
+++ build.ts
@@ -9,13 +9,8 @@ import { languages, parentLang, toAlpha2Code, toAlpha3Code, variantSeperator } f
 import Config from './config';
 import locales from './src/strings/locales.json';
 
-const DIST = 'dist/';
-const STATIC_FILES = ['favicon.ico'];
-
 const prod = process.argv.includes('--prod');
 const watch = process.argv.includes('--watch');
-
-const version = child_process.execSync('git describe --tags --always').toString().trim();
 
 const { allowedLangs, allowedVariants, defaultLocale } = Config;
 
@@ -27,9 +22,9 @@ const apyGet = async (path: string, params: unknown = {}): Promise<AxiosResponse
     validateStatus: (status) => status === 200,
   });
 
-const writeSitemap = async () => {
+const writeSitemap = async (outdir: string) => {
   await fs.writeFile(
-    path.join(DIST, 'sitemap.xml'),
+    path.join(outdir, 'sitemap.xml'),
     `
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
@@ -63,141 +58,159 @@ const allowedLang = (code: string): boolean => {
   return true;
 };
 
-void (async () => {
-  let defaultStrings: unknown;
+const Plugin = {
+  name: 'apertium-html-tools',
+  setup: async ({ initialOptions, onEnd }: esbuild.PluginBuild) => {
+    const version = child_process.execSync('git describe --tags --always').toString().trim();
 
-  const indexHtml = await fs.readFile('src/index.html', 'utf-8');
+    let defaultStrings: unknown;
 
-  const [pairsResponse, analyzersResponse, generatorsResponse] = await Promise.all([
-    apyGet('list', {}),
-    apyGet('list', { q: 'analyzers' }),
-    apyGet('list', { q: 'generators' }),
-  ]);
+    const [pairsResponse, analyzersResponse, generatorsResponse] = await Promise.all([
+      apyGet('list', {}),
+      apyGet('list', { q: 'analyzers' }),
+      apyGet('list', { q: 'generators' }),
+    ]);
 
-  const pairs = (pairsResponse.data as {
-    responseData: Array<{
-      sourceLanguage: string;
-      targetLanguage: string;
-    }>;
-  }).responseData.filter(
-    ({ sourceLanguage, targetLanguage }) => allowedLang(sourceLanguage) && allowedLang(targetLanguage),
-  );
-  const analyzers = Object.fromEntries(
-    Object.entries(analyzersResponse.data as Record<string, string>).filter(([code]) => allowedLang(code)),
-  );
-  const generators = Object.fromEntries(
-    Object.entries(generatorsResponse.data as Record<string, string>).filter(([code]) => allowedLang(code)),
-  );
+    const pairs = (pairsResponse.data as {
+      responseData: Array<{
+        sourceLanguage: string;
+        targetLanguage: string;
+      }>;
+    }).responseData.filter(
+      ({ sourceLanguage, targetLanguage }) => allowedLang(sourceLanguage) && allowedLang(targetLanguage),
+    );
+    const analyzers = Object.fromEntries(
+      Object.entries(analyzersResponse.data as Record<string, string>).filter(([code]) => allowedLang(code)),
+    );
+    const generators = Object.fromEntries(
+      Object.entries(generatorsResponse.data as Record<string, string>).filter(([code]) => allowedLang(code)),
+    );
 
-  let pairPrefs = {};
-  try {
-    pairPrefs = (await apyGet('pairprefs')).data as Record<string, unknown>;
-  } catch (error) {
-    console.warn('Unable to fetch pair prefs, defaulting to empty');
-  }
+    let pairPrefs = {};
+    try {
+      pairPrefs = (await apyGet('pairprefs')).data as Record<string, unknown>;
+    } catch (error) {
+      console.warn('Unable to fetch pair prefs, defaulting to empty');
+    }
 
-  let allLangs: Array<string | null> = [
-    ...pairs.flatMap(({ sourceLanguage, targetLanguage }) => [sourceLanguage, targetLanguage]),
-    ...Object.keys(analyzers),
-    ...Object.keys(generators),
-    ...Object.keys(languages),
-    ...Object.keys(locales),
-  ].filter(Boolean);
-  allLangs = [...allLangs, ...allLangs.map((l) => (l ? parentLang(l) : l))];
-  allLangs = [...allLangs, ...allLangs.map(toAlpha2Code)];
-  allLangs = [...allLangs, ...allLangs.map(toAlpha3Code)];
-  const allLangsSet = new Set(allLangs.filter(Boolean));
+    let allLangs: Array<string | null> = [
+      ...pairs.flatMap(({ sourceLanguage, targetLanguage }) => [sourceLanguage, targetLanguage]),
+      ...Object.keys(analyzers),
+      ...Object.keys(generators),
+      ...Object.keys(languages),
+      ...Object.keys(locales),
+    ].filter(Boolean);
+    allLangs = [...allLangs, ...allLangs.map((l) => (l ? parentLang(l) : l))];
+    allLangs = [...allLangs, ...allLangs.map(toAlpha2Code)];
+    allLangs = [...allLangs, ...allLangs.map(toAlpha3Code)];
+    const allLangsSet = new Set(allLangs.filter(Boolean));
 
-  const localeStrings = (await Promise.all(
-    (await fs.readdir('src/strings'))
-      .filter((f) => f.endsWith('.json') && f !== 'locales.json')
-      .map(async (f) => {
-        const locale = path.parse(f).name;
-        const response = await apyGet('listLanguageNames', { locale });
-        const allLangNames = response.data as Record<string, string>;
+    const outdir = initialOptions.outdir as string;
 
-        const srcPath = path.join('src/strings', f);
-        const distPath = path.join(DIST, 'strings', f);
+    const localeStrings = (await Promise.all(
+      (await fs.readdir('src/strings'))
+        .filter((f) => f.endsWith('.json') && f !== 'locales.json')
+        .map(async (f) => {
+          const locale = path.parse(f).name;
+          const response = await apyGet('listLanguageNames', { locale });
+          const allLangNames = response.data as Record<string, string>;
 
-        await fs.mkdir(path.dirname(distPath), { recursive: true });
+          const srcPath = path.join('src/strings', f);
+          const distPath = path.join(outdir, 'strings', f);
 
-        const strings = JSON.parse(await fs.readFile(srcPath, 'utf-8')) as Record<string, unknown>;
-        delete strings['@metadata'];
+          await fs.mkdir(path.dirname(distPath), { recursive: true });
 
-        const langNames: Record<string, string> = {};
-        for (const lang of Object.keys(allLangNames)) {
-          if (allLangsSet.has(lang)) {
-            langNames[lang] = allLangNames[lang];
+          const strings = JSON.parse(await fs.readFile(srcPath, 'utf-8')) as Record<string, unknown>;
+          delete strings['@metadata'];
+
+          const langNames: Record<string, string> = {};
+          for (const lang of Object.keys(allLangNames)) {
+            if (allLangsSet.has(lang)) {
+              langNames[lang] = allLangNames[lang];
+            }
           }
-        }
-        strings['@langNames'] = langNames;
+          strings['@langNames'] = langNames;
 
-        if (f === `${defaultLocale}.json`) {
-          defaultStrings = strings;
-        }
+          if (f === `${defaultLocale}.json`) {
+            defaultStrings = strings;
+          }
 
-        await fs.writeFile(distPath, JSON.stringify(strings));
+          await fs.writeFile(distPath, JSON.stringify(strings));
 
-        return [locale, strings];
-      }),
-  )) as Array<[string, unknown]>;
+          return [locale, strings];
+        }),
+    )) as Array<[string, unknown]>;
 
-  await Promise.all(
-    localeStrings.map(([locale, strings]) =>
-      fs.writeFile(
-        path.join(DIST, `index.${locale}.html`),
-        indexHtml.replace(
-          '{{PRELOADED_STRINGS}}',
-          JSON.stringify({ [defaultLocale]: defaultStrings, [locale]: strings }),
-        ),
-      ),
-    ),
-  );
+    await writeSitemap(outdir);
 
-  await Promise.all(STATIC_FILES.map((f) => fs.copyFile(path.join('src', f), path.join(DIST, f))));
-
-  await fs.writeFile(
-    path.join(DIST, 'index.html'),
-    indexHtml.replace('{{PRELOADED_STRINGS}}', JSON.stringify({ [defaultLocale]: defaultStrings })),
-  );
-
-  await writeSitemap();
-
-  // TODO: Switch `yarn serve` to use `esbuild.serve` which prevents stale
-  // responses and minimizes FS writes.
-  await esbuild.build({
-    entryPoints: ['src/app.tsx'],
-    bundle: true,
-    metafile: true,
-    loader: { '.embed.png': 'dataurl', '.png': 'file', '.gif': 'file', '.jpg': 'file' },
-    outfile: path.join(DIST, 'bundle.js'),
-
-    define: {
+    initialOptions.define = {
       'window.VERSION': JSON.stringify(version),
       'window.PAIRS': JSON.stringify(pairs),
       'window.PAIR_PREFS': JSON.stringify(pairPrefs),
       'window.ANALYZERS': JSON.stringify(analyzers),
       'window.GENERATORS': JSON.stringify(generators),
-    },
+      ...initialOptions.define,
+    };
 
-    minify: prod,
-    sourcemap: prod,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    onEnd(
+      async (): Promise<undefined> => {
+        const indexHtml = await fs.readFile('src/index.html', 'utf-8');
 
-    incremental: watch,
-    watch: watch
-      ? {
-          onRebuild(error: esbuild.BuildFailure | null, result: esbuild.BuildResult | null) {
-            if (error) console.error('❌ watch build failed');
-            else {
-              console.log('✅ watch build succeeded');
-              if (result) {
-                void (async () => {
-                  await fs.writeFile('meta.json', JSON.stringify(result.metafile));
-                })();
-              }
+        await Promise.all([
+          fs.copyFile('src/favicon.ico', path.join(outdir, 'favicon.ico')),
+          fs.writeFile(
+            path.join(outdir, 'index.html'),
+            indexHtml
+              .replace('{{PRELOADED_STRINGS}}', JSON.stringify({ [defaultLocale]: defaultStrings }))
+              .replace('{{CACHE_BREAK}}', Date.now().toString()),
+          ),
+          ...localeStrings.map(([locale, strings]) =>
+            fs.writeFile(
+              path.join(outdir, `index.${locale}.html`),
+              indexHtml
+                .replace(
+                  '{{PRELOADED_STRINGS}}',
+                  JSON.stringify({ [defaultLocale]: defaultStrings, [locale]: strings }),
+                )
+                .replace(/{{CACHE_BREAK}}/g, Date.now().toString()),
+            ),
+          ),
+        ]);
+
+        return;
+      },
+    );
+  },
+};
+
+// TODO: Switch `yarn serve` to use `esbuild.serve` which prevents stale
+// responses and minimizes FS writes.
+void esbuild.build({
+  entryPoints: ['src/app.tsx'],
+  bundle: true,
+  metafile: true,
+  loader: { '.embed.png': 'dataurl', '.png': 'file', '.gif': 'file', '.jpg': 'file' },
+  outdir: 'dist',
+  plugins: [Plugin],
+
+  minify: prod,
+  sourcemap: prod,
+
+  incremental: watch,
+  watch: watch
+    ? {
+        onRebuild(error: esbuild.BuildFailure | null, result: esbuild.BuildResult | null) {
+          if (error) console.error('❌ watch build failed');
+          else {
+            console.log('✅ watch build succeeded');
+            if (result) {
+              void (async () => {
+                await fs.writeFile('meta.json', JSON.stringify(result.metafile));
+              })();
             }
-          },
-        }
-      : undefined,
-  });
-})();
+          }
+        },
+      }
+    : undefined,
+});
