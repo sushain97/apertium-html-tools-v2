7fcd5e4e7166e3467d910f079adc0186a8ffff3f
Sushain Cherivirala <sushain@skc.name>
2021-05-02 17:48:41 -0700

    Add coverage thresholds

--- jest.config.ts
+++ jest.config.ts
@@ -13,7 +13,17 @@ export default {
     '\\.(css)$': 'identity-obj-proxy',
     '\\.(gif|png|jpg)$': '<rootDir>/src/__mocks__/file.ts',
   },
+
   setupFiles: ['./src/testSetup.ts'],
   setupFilesAfterEnv: ['./src/testEnvSetup.ts'],
+
   collectCoverageFrom: ['src/**/*.ts', 'src/**/*.tsx'],
+  coverageThreshold: {
+    global: {
+      branches: 20,
+      functions: 30,
+      lines: 40,
+      statements: 40,
+    },
+  },
 } as Config.InitialOptions;
